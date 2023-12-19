af9cae811cdae040530d5cdc2a1ff6d595c5f41d
fangliangchen <fangliangchen@protonmail.com>
2023-12-19 12:21:55 +0800

    Initial Commit

---
+++ Lyrics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lyrics
+//
+//  Created by Fang Liangchen on 2023/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lyrics/LyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsApp.swift
+//  Lyrics
+//
+//  Created by Fang Liangchen on 2023/12/19.
+//
+
+import SwiftUI
+
+@main
+struct LyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
