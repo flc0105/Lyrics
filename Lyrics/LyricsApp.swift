1c13a30defda7dc72e35ff88df520a6a74623024
fangliangchen <fangliangchen@protonmail.com>
2023-12-19 13:52:27 +0800

    UI initialization

--- Lyrics/LyricsApp.swift
+++ Lyrics/LyricsApp.swift
@@ -7,11 +7,319 @@
 
 import SwiftUI
 
-@main
-struct LyricsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+
+import Foundation
+
+/// Lyrics parser for handling LRC (Lyric) files.
+class LRCParser {
+    var lyrics: [LyricInfo] = []
+    
+    /// Initializes the parser with the content of an LRC file.
+    /// - Parameter lrcContent: Content of the LRC file.
+    init(lrcContent: String) {
+        parseLRCContent(lrcContent)
+    }
+    
+    /// Parses the content of an LRC file.
+    /// - Parameter content: Content of the LRC file as a string.
+    func parseLRCContent(_ content: String) {
+        // Split the content into lines
+        let lines = content.components(separatedBy: "\n")
+        // Use regular expression to match timestamps
+        let regex = try! NSRegularExpression(pattern: "\\[([0-9]+:[0-9]+.[0-9]+)\\]", options: [])
+        
+        for line in lines {
+            // Find matches for timestamps in each line
+            let matches = regex.matches(in: line, options: [], range: NSRange(location: 0, length: line.utf16.count))
+            if let match = matches.first {
+                // Extract the time string
+                let timeString = (line as NSString).substring(with: match.range(at: 1))
+                // Split the time string into minutes and seconds
+                let timeComponents = timeString.components(separatedBy: ":")
+                if let minutes = Double(timeComponents[0]), let seconds = Double(timeComponents[1]) {
+                    // Calculate the timestamp
+                    let timestamp = minutes * 60 + seconds
+                    // Extract the lyric text
+                    let lyricText = line.replacingOccurrences(of: "\\[.*\\]", with: "", options: .regularExpression, range: nil)
+                    
+                    // Create a LyricInfo instance and add it to the array
+                    let lyricInfo = LyricInfo(id: lyrics.count, text: lyricText, isCurrent: false, playbackTime: timestamp)
+                    lyrics.append(lyricInfo)
+                }
+            }
+        }
+        
+        // Sort the lyrics array based on timestamps
+        lyrics.sort { $0.playbackTime < $1.playbackTime }
+    }
+    
+    /// Gets the parsed lyrics as an array of LyricInfo.
+    /// - Returns: An array of LyricInfo instances representing the lyrics.
+    func getLyrics() -> [LyricInfo] {
+        return lyrics
+    }
+}
+
+
+/// LyricInfo structure representing information for a single line of lyrics.
+struct LyricInfo: Identifiable {
+    /// Unique identifier for the lyric line.
+    let id: Int
+    /// The text of the lyric line.
+    let text: String
+    /// A boolean indicating whether the lyric line is currently being played.
+    var isCurrent: Bool
+    /// The playback time associated with the lyric line.
+    var playbackTime: TimeInterval
+}
+
+/// View model for managing lyrics data.
+class LyricsViewModel: ObservableObject {
+    /// Published property holding the array of LyricInfo representing the lyrics.
+    @Published var lyrics: [LyricInfo] = []
+    
+    /// Updates the lyrics with a new set of lyrics.
+    /// - Parameter newLyrics: The new array of LyricInfo representing the updated lyrics.
+    func updateLyrics(newLyrics: [LyricInfo]) {
+        lyrics = newLyrics
+    }
+}
+
+/// The main view model instance for managing lyrics.
+var viewModel = LyricsViewModel()
+
+/// The start time for tracking the playback time.
+var startTime: TimeInterval = 0
+
+/// A boolean indicating whether the lyrics display is stopped.
+var isStopped: Bool = false
+
+/// AppDelegate class responsible for managing the application's lifecycle.
+class AppDelegate: NSObject, NSApplicationDelegate {
+    /// The main window of the application.
+    var window: NSWindow!
+    
+    /// Called when the application finishes launching.
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Set the start time
+        startTime = Date.timeIntervalSinceReferenceDate
+        
+        // Initialize the lyrics with a default "Not playing" line
+        viewModel.lyrics =  [
+            LyricInfo(id: 0, text: "Not playing", isCurrent: false, playbackTime: 0),
+        ]
+        
+        // Initialize the application
+        registerNotifications()
+        
+        // Create and configure the main window
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 300, height: 400),
+            styleMask: [.titled, .closable, .miniaturizable],
+            backing: .buffered, defer: false)
+        
+        // Create a visual effect view for the window
+        let visualEffectView = NSVisualEffectView()
+        visualEffectView.blendingMode = .behindWindow
+        visualEffectView.state = .active
+        visualEffectView.material = .dark
+        
+        // Create a hosting view for the SwiftUI content
+        let hostingView = NSHostingView(rootView: LyricsView())
+        hostingView.translatesAutoresizingMaskIntoConstraints = false
+        visualEffectView.addSubview(hostingView)
+        
+        // Set up constraints for the hosting view
+        NSLayoutConstraint.activate([
+            hostingView.leadingAnchor.constraint(equalTo: visualEffectView.leadingAnchor),
+            hostingView.trailingAnchor.constraint(equalTo: visualEffectView.trailingAnchor),
+            hostingView.topAnchor.constraint(equalTo: visualEffectView.topAnchor),
+            hostingView.bottomAnchor.constraint(equalTo: visualEffectView.bottomAnchor)
+        ])
+        
+        // Set the content view of the window to the visual effect view
+        window.contentView = visualEffectView
+        window.titlebarAppearsTransparent = true
+        window.styleMask.insert(.fullSizeContentView)
+        window.standardWindowButton(NSWindow.ButtonType.zoomButton)!.isHidden = true
+        window.standardWindowButton(NSWindow.ButtonType.miniaturizeButton)!.isHidden = true
+        window.center()
+        window.setFrameAutosaveName("LyricsWindow")
+        window.makeKeyAndOrderFront(nil)
+        window.styleMask.remove(.resizable)
+    }
+    
+}
+
+/// SwiftUI view representing the lyrics interface.
+struct LyricsView: View {
+    
+    @State private var currentIndex: Int = 0
+    
+    var body: some View {
+        
+        VStack {
+            ScrollView {
+                ScrollViewReader { proxy in
+                    VStack(spacing: 10) {
+                        ForEach(viewModel.lyrics) { lyric in
+                            Text(lyric.text)
+                                .font(lyric.isCurrent ? .system(size: 14) : .system(size: 14))
+                                .foregroundColor(lyric.isCurrent ? .blue : .white)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .id(lyric.id)
+                        }
+                    }
+                    .onChange(of: currentIndex) { newValue in
+                        // Scroll to the current lyric's position
+                        withAnimation() {
+                            proxy.scrollTo(currentIndex, anchor: .center)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                startTimer()
+            }
+        }
+    }
+    
+    /// Start a timer to update lyrics every second.
+    private func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            if !isStopped {
+                updateLyrics()
+            }
+        }
+    }
+    
+    /// Update the lyrics based on the current playback time.
+    private func updateLyrics() {
+        guard !isStopped else {
+            return
+        }
+        
+        let currentPlaybackTime = Date().timeIntervalSinceReferenceDate - startTime
+        
+        for (index, lyric) in viewModel.lyrics.enumerated() {
+            if currentPlaybackTime >= lyric.playbackTime {
+                currentIndex = index
+            }
+        }
+        
+        viewModel.lyrics.indices.forEach { index in
+            viewModel.lyrics[index].isCurrent = (index == currentIndex)
+        }
+        
+        // Check if there is a next lyric
+        let nextIndex = (currentIndex + 1) % viewModel.lyrics.count
+        if nextIndex != currentIndex {
+            let currentLyric = viewModel.lyrics[currentIndex]
+            let nextLyric = viewModel.lyrics[nextIndex]
+            
+            // Calculate the delay time
+            let delay = nextLyric.playbackTime - currentLyric.playbackTime
+            
+            // Use asynchronous delay to continue displaying lyrics
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                // Update the next lyric
+                updateLyrics()
+            }
+        }
+    }
+    
+}
+
+
+/// Starts displaying lyrics for the currently playing track.
+func startLyrics() {
+    // Record the start time of lyric display
+    let lyricStartTime = Date().timeIntervalSinceReferenceDate
+    
+    // Reset the stopped flag
+    isStopped = false
+    
+    // Retrieve now playing information
+    getNowPlayingInfo { nowPlayingInfo in
+        // Check if the now playing information is empty
+        guard !nowPlayingInfo.isEmpty else {
+            print("Now playing information is empty.")
+            return
+        }
+        
+        // Get playback time
+        guard let playbackTime = nowPlayingInfo["ElapsedTime"] as? TimeInterval else {
+            print("Failed to get playback time.")
+            return
+        }
+        
+        // Extract artist and title
+        let artist = nowPlayingInfo["Artist"] as? String ?? ""
+        let title = nowPlayingInfo["Title"] as? String ?? ""
+        
+        // Get the path of the lyrics file
+        let lrcPath = getLRCPath(artist: artist, title: title)
+        
+        // Try to read the contents of the lyrics file
+        if let lrcContent = try? String(contentsOfFile: lrcPath) {
+            print("Lyrics file loaded: \(lrcPath)")
+            
+            // Create an LRC parser
+            let parser = LRCParser(lrcContent: lrcContent)
+            
+            // Get the parsed lyrics array
+            let lyrics = parser.getLyrics()
+            
+            // Set the start time for lyrics display
+            startTime = Date().timeIntervalSinceReferenceDate - playbackTime
+            
+            // Update the lyrics in the view model
+            viewModel.updateLyrics(newLyrics: lyrics)
+            
+            // 3 seconds later, update and calibrate the playback time
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                getNowPlayingInfo { nowPlayingInfo in
+                    guard var updatedPlaybackTime = nowPlayingInfo["ElapsedTime"] as? TimeInterval else {
+                        print("Failed to update playback time.")
+                        return
+                    }
+                    
+                    // Calculate the time gap
+                    let gap = Date().timeIntervalSinceReferenceDate - lyricStartTime
+                    updatedPlaybackTime = updatedPlaybackTime + gap
+                    startTime = Date.timeIntervalSinceReferenceDate - updatedPlaybackTime
+                    print("Playback time updated: \(updatedPlaybackTime)")
+                }
+            }
+        } else {
+            print("Failed to read LRC file.")
+            return
         }
     }
 }
+
+/// Stops displaying lyrics for the currently playing track.
+func stopLyrics() {
+    // Set the stopped flag
+    isStopped = true
+}
+
+
+/// The main entry point for the LyricsApp.
+@main
+struct LyricsApp: App {
+    
+    /// The app delegate for managing the application's lifecycle.
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    /// The body of the app scene.
+    var body: some Scene {
+        // Settings scene
+        Settings {
+            EmptyView()
+        }
+    }
+}
+
+
+
--- Lyrics/MediaRemoteController.swift
+++ Lyrics/MediaRemoteController.swift
@@ -6,3 +6,228 @@
 //
 
 import Foundation
+import Cocoa
+
+// Create a bundle for the MediaRemote framework
+let bundle = CFBundleCreate(kCFAllocatorDefault, NSURL(fileURLWithPath: "/System/Library/PrivateFrameworks/MediaRemote.framework"))
+
+// Get function pointer for MRMediaRemoteGetNowPlayingInfo
+let MRMediaRemoteGetNowPlayingInfoPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteGetNowPlayingInfo" as CFString)
+typealias MRMediaRemoteGetNowPlayingInfoFunction = @convention(c) (DispatchQueue, @escaping ([String: Any]) -> Void) -> Void
+let MRMediaRemoteGetNowPlayingInfo = unsafeBitCast(MRMediaRemoteGetNowPlayingInfoPointer, to: MRMediaRemoteGetNowPlayingInfoFunction.self)
+
+// Get function pointer for MRMediaRemoteGetNowPlayingApplicationIsPlaying
+let MRMediaRemoteGetNowPlayingApplicationIsPlayingPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteGetNowPlayingApplicationIsPlaying" as CFString)
+typealias MRMediaRemoteGetNowPlayingApplicationIsPlayingFunction = @convention(c) (DispatchQueue, @escaping (Bool) -> Void) -> Void
+let MRMediaRemoteGetNowPlayingApplicationIsPlaying = unsafeBitCast(MRMediaRemoteGetNowPlayingApplicationIsPlayingPointer, to: MRMediaRemoteGetNowPlayingApplicationIsPlayingFunction.self)
+
+// Get function pointer for MRMediaRemoteRegisterForNowPlayingNotifications
+let MRMediaRemoteRegisterForNowPlayingNotificationsPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteRegisterForNowPlayingNotifications" as CFString)
+typealias MRMediaRemoteRegisterForNowPlayingNotificationsFunction = @convention(c) (DispatchQueue) -> Void
+let MRMediaRemoteRegisterForNowPlayingNotifications = unsafeBitCast(MRMediaRemoteRegisterForNowPlayingNotificationsPointer, to: MRMediaRemoteRegisterForNowPlayingNotificationsFunction.self)
+
+// Get function pointer for MRMediaRemoteUnregisterForNowPlayingNotifications
+let MRMediaRemoteUnregisterForNowPlayingNotificationsPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteUnregisterForNowPlayingNotifications" as CFString)
+typealias MRMediaRemoteUnregisterForNowPlayingNotificationsFunction = @convention(c) (DispatchQueue) -> Void
+let MRMediaRemoteUnregisterForNowPlayingNotifications = unsafeBitCast(MRMediaRemoteUnregisterForNowPlayingNotificationsPointer, to: MRMediaRemoteUnregisterForNowPlayingNotificationsFunction.self)
+
+
+/// Retrieves information about the currently playing media.
+///
+/// - Parameters:
+///   - completion: A closure to be called once the now playing information is obtained.
+///                  The closure takes a dictionary containing information such as artist,
+///                  title, and elapsed time as parameters.
+func getNowPlayingInfo(completion: @escaping ([String: Any]) -> Void) {
+    var nowPlayingInfo: [String: Any] = [:]
+    
+    // Call the Media Remote framework to get now playing information
+    MRMediaRemoteGetNowPlayingInfo(DispatchQueue.main) { information in
+        
+        // Check if the information is empty
+        if information.isEmpty {
+            print("Could not find the specified now playing client")
+            // Call the completion handler with an empty dictionary
+            completion(nowPlayingInfo)
+            return
+        }
+        
+        // Extract information from the result
+        nowPlayingInfo["Artist"] = information["kMRMediaRemoteNowPlayingInfoArtist"] as? String ?? ""
+        nowPlayingInfo["Title"] = information["kMRMediaRemoteNowPlayingInfoTitle"] as? String ?? ""
+        nowPlayingInfo["ElapsedTime"] = information["kMRMediaRemoteNowPlayingInfoElapsedTime"] as? TimeInterval ?? 0.0
+        
+        // Call the completion handler with the updated dictionary
+        completion(nowPlayingInfo)
+    }
+}
+
+
+/// Retrieves the playback state of the currently playing media application.
+///
+/// - Parameter completion: A closure to be called once the playback state is obtained.
+///                         The closure takes a boolean parameter indicating whether
+///                         the media application is currently playing or not.
+func getPlaybackState(completion: @escaping (Bool) -> Void) {
+    // Call the Media Remote framework to get the playback state
+    MRMediaRemoteGetNowPlayingApplicationIsPlaying(DispatchQueue.main) { isPlaying in
+        // Call the completion handler with the obtained playback state
+        completion(isPlaying)
+    }
+}
+
+
+var currentTrack: String?
+
+/// Handles the notification when the now playing information changes.
+///
+/// - Parameter notification: The notification object.
+func handleNowPlayingInfoDidChangeNotification(notification: Notification) {
+    getNowPlayingInfo { nowPlayingInfo in
+        
+        // Check if the now playing information is empty
+        guard !nowPlayingInfo.isEmpty else {
+            print("Now playing information is empty.")
+            return
+        }
+        
+        // Extract artist and title
+        let artist = nowPlayingInfo["Artist"] as? String ?? ""
+        let title = nowPlayingInfo["Title"] as? String ?? ""
+        let track = "\(artist) - \(title)"
+        
+        // Check if the current track is nil or if it's the same as the new track
+        if currentTrack == nil || track == currentTrack {
+            return
+        } else {
+            print("Track change detected: \(String(describing: currentTrack)) -> \(track)")
+            currentTrack = track
+            
+            // Check playback state
+            getPlaybackState { isPlaying in
+                if isPlaying {
+                    // Take action when the playback state is playing
+                }
+            }
+        }
+    }
+}
+
+/// Constructs and returns the path for the LRC (Lyrics) file based on the artist and title of the song.
+///
+/// - Parameters:
+///   - artist: The artist of the song.
+///   - title: The title of the song.
+/// - Returns: The file path for the LRC file.
+func getLRCPath(artist: String, title: String) -> String {
+    // Create the file name by combining artist and title
+    let fileName = "\(artist) - \(title).lrc"
+    
+    // Replace illegal characters in the file name with underscores
+    let illegalCharacters = CharacterSet(charactersIn: "\\/:*?\"<>|")
+    let replacedFileName = fileName.components(separatedBy: illegalCharacters).joined(separator: "_")
+    
+    // Construct and return the full file path
+    return "/Users/flc/Desktop/Lyrics/\(replacedFileName)"
+}
+
+/// Enum representing different playback states for a media application.
+enum PlaybackState: Int {
+    /// The media application is terminated.
+    case terminated = 0
+    /// The media application is currently playing.
+    case playing = 1
+    /// The media application is paused.
+    case paused = 2
+    /// The media application is stopped.
+    case stopped = 3
+}
+
+/// Handles the change in playback state of the currently playing media application.
+///
+/// - Parameter notification: The notification object containing information about the playback state change.
+func handleNowPlayingApplicationPlaybackStateDidChange(notification: Notification) {
+    // Extract the raw playback state from the notification's user info
+    let rawPlaybackState = notification.userInfo?["kMRMediaRemotePlaybackStateUserInfoKey"] as? Int ?? 3
+    
+    // Convert the raw playback state to a PlaybackState enum
+    if let playbackState = PlaybackState(rawValue: rawPlaybackState) {
+        // Print the detected playback state
+        print("Playback state change detected: \(playbackState)")
+        
+        // Check the playback state and perform actions accordingly
+        if playbackState == .playing {
+            startLyrics()
+        } else {
+            stopLyrics()
+        }
+    }
+}
+
+
+/// Register notifications for Now Playing info and application playback state changes.
+func registerNotifications() {
+    
+    // Bundle Identifier of the application to be monitored
+    let targetAppBundleIdentifier = "com.roon.Roon"
+    
+    let notificationCenter = NotificationCenter.default
+    
+    // Register for Now Playing info change notification
+    notificationCenter.addObserver(forName: Notification.Name("kMRMediaRemoteNowPlayingInfoDidChangeNotification"),
+                                   object: nil,
+                                   queue: nil,
+                                   using: handleNowPlayingInfoDidChangeNotification)
+    
+    // Register for application playback state change notification
+    notificationCenter.addObserver(forName: Notification.Name("kMRMediaRemoteNowPlayingApplicationPlaybackStateDidChangeNotification"),
+                                   object: nil,
+                                   queue: nil,
+                                   using: handleNowPlayingApplicationPlaybackStateDidChange)
+    
+    let center = NSWorkspace.shared.notificationCenter
+    
+    // Register for the launch of an application notification
+    center.addObserver(forName: NSWorkspace.didLaunchApplicationNotification,
+                       object: nil,
+                       queue: OperationQueue.main) { (notification: Notification) in
+        if let app = notification.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication {
+            if app.bundleIdentifier == targetAppBundleIdentifier {
+                // Register Now Playing notifications
+                MRMediaRemoteRegisterForNowPlayingNotifications(DispatchQueue.main)
+                print("MediaRemote now playing notifications registered.")
+            }
+        }
+    }
+    
+    // Register for the termination of an application notification
+    center.addObserver(forName: NSWorkspace.didTerminateApplicationNotification,
+                       object: nil,
+                       queue: OperationQueue.main) { (notification: Notification) in
+        if let app = notification.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication {
+            if app.bundleIdentifier == targetAppBundleIdentifier {
+                // Unregister Now Playing notifications
+                MRMediaRemoteUnregisterForNowPlayingNotifications(DispatchQueue.main)
+                print("MediaRemote now playing notifications unregistered.")
+            }
+        }
+    }
+    
+    // Check if the application is already running at startup
+    if let targetApp = NSRunningApplication.runningApplications(withBundleIdentifier: targetAppBundleIdentifier).first {
+        print("Application is running: \(targetApp.localizedName ?? "").")
+        
+        // Register Now Playing notifications
+        MRMediaRemoteRegisterForNowPlayingNotifications(DispatchQueue.main)
+        print("MediaRemote now playing notifications registered.")
+        
+        // Check the current playback state at startup
+        getPlaybackState { isPlaying in
+            if isPlaying {
+                // Handle the case when the application is already playing
+            }
+        }
+    } else {
+        print("Application is not running.")
+    }
+}
